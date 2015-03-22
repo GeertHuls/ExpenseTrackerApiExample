0ff07399d5ed1fddd78d91f9e591623494badf26
Geert Huls <geert.huls@macadam.eu>
2015-03-22 22:11:24 +0100

    Repository and dto classes

---
+++ ExpenseTracker/ExpenseTracker.Repository/Factories/ExpenseFactory.cs
@@ -0,0 +1,38 @@
+﻿using ExpenseTracker.DTO;
+
+namespace ExpenseTracker.Repository.Factories
+{
+    public class ExpenseFactory
+    {
+
+        public ExpenseFactory()
+        {
+
+        }
+
+        public Expense CreateExpense(Entities.Expense expense)
+        {
+            return new Expense()
+            {
+                Amount = expense.Amount,
+                Date = expense.Date,
+                Description = expense.Description,
+                ExpenseGroupId = expense.ExpenseGroupId,
+                Id = expense.Id
+            };
+        }
+
+        public Entities.Expense CreateExpense(Expense expense)
+        {
+            return new Entities.Expense()
+            {
+                Amount = expense.Amount,
+                Date = expense.Date,
+                Description = expense.Description,
+                ExpenseGroupId = expense.ExpenseGroupId,
+                Id = expense.Id
+            };
+        }
+         
+    }
+}
