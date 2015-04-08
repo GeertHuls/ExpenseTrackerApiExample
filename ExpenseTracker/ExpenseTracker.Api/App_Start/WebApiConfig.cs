13f0f49dc9f9b7074307f3915e7b67b6329dc890
Geert Huls <geert.huls@macadam.eu>
2015-04-08 21:52:16 +0200

    Using CacheChow to generate server side ETags

--- ExpenseTracker/ExpenseTracker.Api/App_Start/WebApiConfig.cs
+++ ExpenseTracker/ExpenseTracker.Api/App_Start/WebApiConfig.cs
@@ -28,6 +28,17 @@ namespace ExpenseTracker.Api
             config.Formatters.JsonFormatter.SerializerSettings.ContractResolver
                 = new CamelCasePropertyNamesContractResolver();
 
+            config.MessageHandlers.Add(new CacheCow.Server.CachingHandler(config));
+            /// Example:
+            /// 1) Get http://localhost:679/api/expensegroups/1 + note the etag in the response header
+            /// 
+            /// 2) Get http://localhost:679/api/expensegroups/1 +
+            /// User-Agent: Fiddler
+            /// Host: localhost:679
+            /// If-None-Match: W/"5d1dd8af7e2c4885a572020ebbe3edaa"
+            /// 
+            /// Reponse = 412 (Not modified)
+
             return config;
         }
     }
