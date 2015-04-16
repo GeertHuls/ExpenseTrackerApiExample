39bd1f75e48fd79b3017e45a780566bcfc7e99ea
Geert Huls <geert.huls@macadam.eu>
2015-04-16 22:45:43 +0200

    Expense group home page

---
+++ ExpenseTracker/ExpenseTracker.WebClient/Helpers/ExpenseTrackerHttpClient.cs
@@ -0,0 +1,22 @@
+﻿using ExpenseTracker.Constants;
+using System;
+using System.Net.Http;
+
+namespace ExpenseTracker.WebClient.Helpers
+{
+    public static class ExpenseTrackerHttpClient
+    {
+
+        public static HttpClient GetClient()
+        {
+            HttpClient client = new HttpClient();
+
+            client.BaseAddress = new Uri(ExpenseTrackerConstants.ExpenseTrackerAPI);
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(
+                new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+
+            return client;
+        }
+    }
+}
