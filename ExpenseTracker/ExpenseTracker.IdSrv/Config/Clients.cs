e17e011f6ff4e39c5342470e28c48e1bd3e34930
Geert Huls <geert.huls@macadam.eu>
2015-04-28 22:33:21 +0200

    Configuring client flow

---
+++ ExpenseTracker/ExpenseTracker.IdSrv/Config/Clients.cs
@@ -0,0 +1,32 @@
+﻿using ExpenseTracker.Constants;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Thinktecture.IdentityServer.Core.Models;
+
+namespace ExpenseTracker.IdSrv.Config
+{
+    public static class Clients
+    {
+        public static IEnumerable<Client> Get()
+        {
+            return new[]
+            {
+                new Client
+                {
+                    Enabled = true,
+                    ClientName = "ExpenseTracker MVC Client (Hybrid Flow)",
+                    ClientId = "mvc",
+                    Flow = Flows.Hybrid,
+                    RequireConsent = true, //ensures that user will see approve consent screen
+
+                    RedirectUris = new List<string>
+                        {
+                            ExpenseTrackerConstants.ExpenseTrackerClient
+                        }
+                }
+            };
+        }
+    }
+}
