c9c411658651bfa2e09ccf990ac2e186a560b484
Geert Huls <geert.huls@macadam.eu>
2015-04-28 22:56:18 +0200

    Configure id server with users, clients and scopes

---
+++ ExpenseTracker/ExpenseTracker.IdSrv/Config/Scopes.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Thinktecture.IdentityServer.Core.Models;
+
+namespace ExpenseTracker.IdSrv.Config
+{
+    public static class Scopes
+    {
+        public static IEnumerable<Scope> Get()
+        {
+            var scopes = new List<Scope>()
+                {
+                    //identity scopes
+                    StandardScopes.OpenId, //to support identity tokens, use OpenId connect
+                    StandardScopes.Profile //used for profile information
+                };
+
+            return scopes;
+        }
+    }
+}
--- ExpenseTracker/ExpenseTracker.IdSrv/Startup.cs
+++ ExpenseTracker/ExpenseTracker.IdSrv/Startup.cs
@@ -1,5 +1,10 @@
-﻿using Microsoft.Owin;
+﻿using ExpenseTracker.Constants;
+using ExpenseTracker.IdSrv.Config;
+using Microsoft.Owin;
 using Owin;
+using System;
+using System.Security.Cryptography.X509Certificates;
+using Thinktecture.IdentityServer.Core.Configuration;
 
 [assembly: OwinStartup(typeof(ExpenseTracker.IdSrv.Startup))]
 
@@ -9,7 +14,30 @@ namespace ExpenseTracker.IdSrv
     {
         public void Configuration(IAppBuilder app)
         {
+            app.Map("/identity", idsrvApp =>
+                {
+                    idsrvApp.UseIdentityServer(
+                        new IdentityServerOptions
+                        {
+                            SiteName = "Embedded IdentityServer",
+                            IssuerUri = ExpenseTrackerConstants.IdSrvIssueUri,
 
+                            Factory = InMemoryFactory.Create(
+                                users: Users.Get(),
+                                clients: Clients.Get(),
+                                scopes: Scopes.Get(),
+                            ),
+                            
+                            SigningCertificate = LoadCertificate()
+                        });
+                });
+        }
+
+        X509Certificate2 LoadCertificate()
+        {
+            return new X509Certificate2(
+                string.Format(@"{0}\Certificates\idsrv3test.pfx",
+                AppDomain.CurrentDomain.BaseDirectory), "idsrv3test");
         }
     }
 }
