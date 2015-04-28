b78678eec45f94e7de49511047b1cecc6303b71d
Geert Huls <geert.huls@macadam.eu>
2015-04-28 22:26:49 +0200

    Created empty identity server web project

---
+++ ExpenseTracker/ExpenseTracker.IdSrv/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(ExpenseTracker.IdSrv.Startup))]
+
+namespace ExpenseTracker.IdSrv
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+
+        }
+    }
+}
